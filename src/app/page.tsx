33cdf5cfcc66eca2cd3b9402f8f146fff3828209
Siddiqa Badar <siddiqaasim778899@gmail.com>
2024-09-29 22:01:34 +0500

    Assignment 3

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+
+
+
+import Hero from "@/components/hero";
+import Navbar from "@/components/navbar";
+import Link from "next/link";
+
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+     
+      
+      
+      
+      
+    </div>
+  );
+}
---
+++ src/components/navbar.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+
+
+const Navbar = () => {
+  return (
+    <div className=" w-[100%]  top-0 z-50">
+      <header className="bg-green-600 text-gray-100 font-serif font-bold body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+            
+            <span className="ml-3 text-2xl text-gray-200 hover:animate-spin">
+              Siddiqa Badar
+            </span>
+          </a>
+          <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+            <Link
+              href="/"
+              className="mr-5 text-xl hover:bg-pink-700 hover:p-3 hover:rounded-2xl cursor-pointer"
+            >
+              Home
+            </Link>
+            <Link
+              href="/about"
+              className="mr-5 text-xl hover:bg-pink-700 hover:p-3 hover:rounded-2xl cursor-pointer"
+            >
+              About Us
+            </Link>
+           
+             
+            
+            <Link
+              href="/contact"
+              className="mr-5 text-xl hover:bg-pink-700 hover:p-3 hover:rounded-2xl cursor-pointer"
+            >
+              Contact Us
+            </Link>
+          </nav>
+         
+        </div>
+      </header>
+    </div>
+  );
+};
+
+export default Navbar;
